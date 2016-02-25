7c137773d1c5a5a9fd782c39c3a642903de8a3f8
TANTAI <taib1210029@gmail.com>
2016-02-25 11:24:38 +0700

    Update http.R

--- R/http.R
+++ R/http.R
@@ -12,7 +12,7 @@ parseHttpUrl <- function(urlText) {
   url <- list()
   url$protocol <- components[[2]]
   url$host <- components[[3]]
-  url$port <- 44311
+  url$port <- 3838
   url$path <- components[[5]]
   url
 }
